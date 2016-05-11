48c6d500bf85f1daa09865bb95d74bb6eaeb509d
yapcheahshen <yapcheahshen@gmail.com>
2016-05-11 23:23:15 +0800

    use background color for diff without note(possible typo)

--- compare-taisho-yinshun.js
+++ compare-taisho-yinshun.js
@@ -12,7 +12,7 @@ var t99=require("./t99");
 var agama_yinshun=require("./agama_yinshun");
 var diffcount=0,samecount=0;
 var normalize={"却":"卻","恒":"恆","麤": "麁","甞":"嘗","犍": "揵","猪":"豬" ,"脚": "腳"
-,"嘆":"歎","栗":"粟","厨":"廚"};
+,"嘆":"歎","栗":"粟","厨":"廚","茲":"玆","舘":"館" };
 
 var removeNormalize=function(diff){
 	var out=[],i=0;
@@ -35,11 +35,30 @@ var countDiff=function(diff){
 	}
 	return ndiff;
 }
+
+var hasNoteAt=function(json,pos) {
+	var p=json.offsets.indexOf(pos);
+	if (p>-1) {
+		return json.tags[p].substr(0,4)=="note";
+	}
+}
+
+var unpack=function(json){ //unpack offset and tags for faster access
+	for (var i in json) {
+		json[i].tags=json[i].tags.split("\1");
+		json[i].offsets=json[i].offsets.split(",").map(function(o){return parseInt(o)});
+	}
+}
+unpack(t99);
+unpack(agama_yinshun);
+
 for (var taisho_sid in t99) {
+	var taisho_offset=0;yinshun_offset=0,hasnote=false;
+
 	var yinshun_sid=taisho_yinshun.taisho2yinshun(taisho_sid);
 	if (!yinshun_sid) continue;
 
-	var diff=jsdiff.diffChars(t99[taisho_sid],agama_yinshun[yinshun_sid]);
+	var diff=jsdiff.diffChars(t99[taisho_sid].text,agama_yinshun[yinshun_sid].text);
 	if (diff.length==1) {
 		samecount++;
 	} else {
@@ -52,15 +71,26 @@ for (var taisho_sid in t99) {
 		diffcount++;
 		console.log("<h2><font color=brown>T"+taisho_sid+"</font>::<font color=green>Y"+yinshun_sid+"</font></h2>");
 		diff.forEach(function(d){
+
 			if (d.added) {
-				console.log("<font color=green>"+d.value+"</font>")
+				yinshun_offset+=d.value.length;
+				hasnote=hasNoteAt(agama_yinshun[yinshun_sid],yinshun_offset);
+
+				var key=hasnote?"color":"background";
+				console.log("<span style='"+key+":green'>"+d.value+"</span>")
 			} else if (d.removed) {
-				console.log("<font color=brown>"+d.value+"</font>");
+				taisho_offset+=d.value.length;
+				hasnote=hasNoteAt(t99[taisho_sid],taisho_offset);
+				//var key=hasnote?"color":"background";
+				console.log("<span style='color:brown'>"+d.value+"</span>");
 			} else {
+				taisho_offset+=d.value.length;
+				yinshun_offset+=d.value.length;
 				console.log(d.value);
 			}
 		});
+		console.log(taisho_offset,yinshun_offset)
 	}
-	
+	//break;
 }
 console.log(samecount,diffcount)
